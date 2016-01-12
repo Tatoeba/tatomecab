69accd7ca980c138aaab97e47b0cc1805f3593f1
時流 <jiru@users.noreply.github.com>
2016-01-12 07:29:53 +0100

    Initial commit
    Basic functionality already here!

---
+++ test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import warifuri
+import unittest
+
+class WarifuriTest(unittest.TestCase):
+    def setUp(self):
+        self.warifuri = warifuri.Warifuri()
+        self.warifuri.readings = {
+            ord('間'): ['かん'],   ord('接'): ['せつ'],
+            ord('男'): ['おとこ'], ord('子'): ['こ'],
+        }
+
+    def assert_split_furi(self, kanjis, readings):
+        result = self.warifuri.split_furi(''.join(kanjis), ''.join(readings))
+        self.assertEqual([kanjis, readings], result)
+
+    def assert_cant_split_furi(self, kanjis, readings):
+        readings = ''.join(readings)
+        kanjis = ''.join(kanjis)
+        result = self.warifuri.split_furi(kanjis, readings)
+        self.assertEqual([(kanjis,), (readings,)], result)
+
+    def test_cant_split(self):
+        expected = [ ('間接',), ('あれあれれ',) ]
+        self.assert_cant_split_furi(('間','接'), ('あれ','あれれ'))
+
+    def test_simple(self):
+        self.assert_split_furi(('間','接'), ('かん','せつ'))
+
+    def test_kana_mix(self):
+        self.assert_split_furi(('男','の','子'), ('おとこ','の','こ'))
+
+    def test_unknown_kanji(self):
+        self.assert_cant_split_furi(('子','供'), ('こ','ども'))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ warifuri.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import xml.etree.ElementTree as ET
+import sys, string, re
+
+class Warifuri():
+    def __init__(self):
+        kata = "ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲン"
+        hira = "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをん"
+        self.kata_to_hira_map = str.maketrans(kata, hira)
+        self.readings = {}
+
+    def kata_to_hira(self, string):
+        return string.translate(self.kata_to_hira_map)
+
+    def load_kanjidic_readings(self, filename):
+        all_readings = {}
+        root = ET.parse(filename).getroot()
+        for character in root.findall('character'):
+            char = character.find('literal').text
+            readings = []
+            for r in character.iter('reading'):
+                if r.get('r_type') in ['ja_on', 'ja_kun']:
+                    reading = r.text.split('.')[0]
+                    readings.append(self.kata_to_hira(reading))
+            if char:
+                self.readings[ord(char)] = readings
+
+    def split_furi(self, kanjis, furi):
+        chars = []
+        n_kanjis = 0
+        for kanji in kanjis:
+            try:
+                readings = self.readings[ord(kanji)]
+            except KeyError:
+                readings = [ kanji ]
+            chars.append('(' + '|'.join(readings) + ')')
+            n_kanjis = n_kanjis + 1
+        regex = '^' + ''.join(chars) + '$'
+        split = re.findall(regex, furi)
+        if (split and len(split[0]) == n_kanjis):
+            return [ tuple(kanjis), split[0] ]
+        else:
+            return [ (kanjis,), (furi,) ]
+
+if __name__ == '__main__':
+    warifuri = Warifuri()
+    warifuri.load_kanjidic_readings(sys.argv[1])
+    print(warifuri.split_furi(sys.argv[2], sys.argv[3]))
