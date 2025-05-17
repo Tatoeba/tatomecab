d4c79e27dfa0cf92eec7a4a2a9b3af07be868aa2
時流 <jiru@users.noreply.github.com>
2025-05-17 10:49:57 +0000

    Fix kanjidict and mecab dict URLs
    Use URLs that are working as of today.

--- warifuri/build.sh
+++ warifuri/build.sh
@@ -9,13 +9,13 @@ maybe_download() {
    fi
 }
 
-maybe_download kanjidic2.xml.gz http://www.csse.monash.edu/~jwb/kanjidic2/kanjidic2.xml.gz
+maybe_download kanjidic2.xml.gz https://www.edrdg.org/kanjidic/kanjidic2.xml.gz
 if [ ! -e kanjidic2.xml ]; then
     gunzip -k kanjidic2.xml.gz
 fi
 
 DICT_BASE_NAME=mecab-naist-jdic-0.6.3b-20111013
-maybe_download "$DICT_BASE_NAME.tar.gz" "http://osdn.jp/frs/redir.php?m=rwthaachen&f=%2Fnaist-jdic%2F53500%2Fmecab-naist-jdic-0.6.3b-20111013.tar.gz"
+maybe_download "$DICT_BASE_NAME.tar.gz" "http://iij.dl.sourceforge.jp/naist-jdic/53500/mecab-naist-jdic-0.6.3b-20111013.tar.gz"
 if [ ! -d "$DICT_BASE_NAME" ]; then
     tar xf "$DICT_BASE_NAME.tar.gz"
 fi
